86de75e9279a058f08f716704a0773a29d221386
Svetlana Shlekht <sv.2502@mail.ru>
2023-08-15 12:20:16 +0300

    fix: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];
+  
+  for (let i=0; i<str.length; i++) {
+    let current = str[i];
+
+    if(isCloseBrackets(current)) {
+        if(bracketsConfig[current] !== stack.pop()) return false;
+    } else {
+      stack.push(current);
+    }
+
+  }
+
+  return stack.length === 0;
+
+  function isCloseBrackets(char) {
+      return [')', '}', ']', '|', '2', '4', '6', '7', '8'].indexOf(char) > -1;
+  }
+
 }
